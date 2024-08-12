4120ce90be55346aa55d6dd81cc7a65d014184bb
Jeevan Singh <jvn.sidhu@gmail.com>
2024-08-11 22:33:43 -0400

    added Search page and test

---
+++ src/test/java/pages/SearchPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class SearchPage {
+
+	public static void main(String[] args) {
+		System.out.println("This is Search Page");
+
+	}
+
+}
---
+++ src/test/java/tests/SearchTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class SearchTest {
+
+	public static void main(String[] args) {
+		System.out.println("Search Test");
+
+	}
+
+}
